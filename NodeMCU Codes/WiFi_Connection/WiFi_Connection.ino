fc49e7b07b6f9119ce7252aa56f413128ab138d5
Sudhanshu Ranjan <77230416+SudhansuuRanjan@users.noreply.github.com>
2021-10-13 20:01:36 +0530

    First Commit

---
+++ NodeMCU Codes/WiFi_Connection/WiFi_Connection.ino
@@ -0,0 +1,19 @@
+#include <ESP8266WiFi.h>
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+   WiFi.begin("POCO","Sudhanshu98");
+   while(WiFi.status() != WL_CONNECTED)
+   {
+    Serial.print("...\n");
+    delay(200);
+   }
+   Serial.println();
+   Serial.println("NodeMCU is Connected!");
+   Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    
+}
